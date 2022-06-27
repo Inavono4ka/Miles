93f582e882c14a69886d0847e5c73220002cf0f2
Mariia <mariya.cheaptrip@gmail.com>
2022-06-27 22:11:00 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 10000;
+        int miles = 20;
+
+        int bonus = amount / miles;
+        System.out.println("Количество миль за купленный билет : " + bonus);
+
+    }
+}
